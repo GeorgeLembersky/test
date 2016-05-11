88a6558c0ef6a2ad8d5146363e521c9657f42324
georgelembersky <glembersky@yahoo.com>
2016-05-11 05:16:09 -0700

    x

--- src/main/java/dudeco/TestController.java
+++ src/main/java/dudeco/TestController.java
@@ -3,12 +3,46 @@ package dudeco;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.*;
+import java.net.*;
+import redis.clients.jedis.*;
+import java.util.*;
 
 @Controller 
 public class TestController {
+	JedisPool pool = null;
 	
+	public TestController() {
+		try {
+        URI redisUri = new URI(System.getenv("REDISCLOUD_URL"));
+        pool = new JedisPool(new JedisPoolConfig(),
+                redisUri.getHost(),
+                redisUri.getPort(),
+                Protocol.DEFAULT_TIMEOUT,
+                redisUri.getUserInfo().split(":",2)[1]);
+		} catch (URISyntaxException e) {
+      throw new RuntimeException(e);  
+		}
+
+	}
+
 	@RequestMapping("/")
-	public ModelAndView testIndex() {
-		return new ModelAndView("testIndex", "model", "my message");
+	public ModelAndView testIndex(
+		@RequestParam(value="string", defaultValue="", required = false) String value,
+		@RequestParam(value="number", defaultValue="0", required = false) Integer score) {
+
+		Jedis jedis = pool.getResource();
+		Map<String,String> map = null;
+		Set<String> set = null;
+		Set<Tuple> tuples = null;
+
+		try {
+			jedis.zadd("key", score, value);
+			tuples = jedis.zrangeWithScores("key", 0, -1);
+		} finally {
+			// return the instance to the pool when you're done
+			pool.returnResource(jedis);	
+		}
+
+		return new ModelAndView("testIndex", "model", tuples);
 	}
 }
--- src/main/webapp/WEB-INF/jsp/testIndex.jsp
+++ src/main/webapp/WEB-INF/jsp/testIndex.jsp
@@ -1,3 +1,32 @@
-hello
-${model}
-done 2 
+<%@ page import="java.util.*,redis.clients.jedis.*" %>
+
+<html>
+	<head>
+	</head>
+	<body>
+
+		<form>
+			String: <input type="text" name="string">
+			<br>
+			Score: <input type="text" name="number">
+			<br>
+			<br>
+			<input text="Add an entry" type=submit>
+		</form>
+
+<br>
+
+<table border=1>
+<tr><th>String</th><th>Score</th></tr>
+<%
+for (Tuple tuple:((Set<Tuple>)request.getAttribute("model"))) {
+	%><tr><td><%=tuple.getElement()%></td><td><%=tuple.getScore()%></td></tr><%
+}
+%>
+</table>
+
+<p>
+
+model = ${model}
+	</body>
+</html>
